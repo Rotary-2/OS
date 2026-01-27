a7ee1c6a1a9a052f649eda6e8c1634bd89fb12a1
Rotary-2 <yjcdghknb@163.com>
2026-01-27 20:50:07 +0800

    实现单个字符打印

---
+++ 6/2/main.c
@@ -0,0 +1,17 @@
+#include "print.h"
+
+void main(void) {
+    put_char('I');
+    put_char(' ');
+    put_char('a');
+    put_char('m');
+    put_char(' ');
+    put_char('k');
+    put_char('e');
+    put_char('r');
+    put_char('n');
+    put_char('e');
+    put_char('l');
+    put_char('\n');
+    while(1); // 死循环防止程序退出
+}
